5ab973f242eeda7aa92817801fc742ff88e6383e
kelvin <edithmukoro3@gmail.com>
2023-05-02 14:01:07 +0000

    initial commit

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,29 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+	def get(self, cls, id):
+		"""A method to retrieve one object without class name and id"""
+			result = None
+			try:
+				objs = self.__session.query(models.classes[cls]).all()
+				for obj in objs:
+					if obj.id == id:
+						result = obj
+			except BaseException:
+			  pass
+			return result
+	
+	def count(self, cls=None):
+		"""A method to count the number of objects in storage"""
+			cls_counter = 0
+
+			if cls is not None:
+				objs = self.__session.query(models.classes[cls]).all()
+				cls_counter = len(objs)
+			else:
+				for i, j in models.classes.items():
+					if i != "BaseModel":
+						objs = self.__session.query(models.classes[i]).all()
+						cls_counter += len(objs)
+			return cls_counter
