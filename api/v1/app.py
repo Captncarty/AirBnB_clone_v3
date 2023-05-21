44ac5b0d8f517b093b3b87b8619e3588dc7b3c85
kelvin <edithmukoro3@gmail.com>
2023-05-21 22:31:21 +0000

    fixed

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,35 @@
 #!/usr/bin/python3
-"""Time to start your API! Your first endpoint (route) returns status of your API:"""
-from os import getenv
-from flask import Flask, jsonify, make_response
+'''
+    app for registering blueprint and starting flask
+'''
+from flask import Flask, make_response, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
+from os import getenv
+
 
 app = Flask(__name__)
-"""Specifies CORS options on a resource and origin level"""
-CORS(app, resources={'/*': {'origins': '0.0.0.0'}})
+CORS(app, origins="0.0.0.0")
 app.register_blueprint(app_views)
+
 
 @app.teardown_appcontext
 def tear_down(self):
-    """close query after each session"""
+    '''
+    close query after each session
+    '''
     storage.close()
-    
+
+
 @app.errorhandler(404)
 def not_found(error):
-    """return JSON formatted 404 status response"""
+    '''
+    return JSON formatted 404 status code response
+    '''
     return make_response(jsonify({'error': 'Not found'}), 404)
+
 
 if __name__ == "__main__":
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True, debug=True)
+            port=int(getenv("HBNB_API_PORT", "5000")), threaded=True)
